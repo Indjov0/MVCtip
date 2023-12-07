614cc63d0e5449dc3d08978a33a36ef291fc6ad7
Gabbyy0511 <totoklik03@gmail.com>
2023-12-07 13:11:12 +0200

    Prime Numbers MVC

---
+++ MVCtip/Model/Tip.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCtip.Model
+{
+	internal class Tip
+	{
+		private double amount;
+		private double percent;
+
+		public double Amount
+		{
+			get { return amount; }
+			set { amount = value; }
+		}
+		public double Percent
+		{
+			get { return percent; }
+			set
+			{
+				if (value > 1)
+				{
+					percent = value / 100.0;
+				}
+				else
+				{
+					percent = value;
+				}
+			}	
+		}
+        public Tip() : this(0, 0)
+        {
+            
+        }
+        public Tip(double amount, double percent)
+        {
+			this.Amount = amount;
+			this.Percent = percent;
+        }
+		public double CalculateTip()
+		{
+			return Amount * Percent;
+		}
+		public double CalculateTotal()
+		{
+			return CalculateTip() + Amount;
+		}
+    }
+}
---
+++ MVCtip/View/Display.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCtip.View
+{
+    internal class Display
+    {
+        public double Amount { get; private set; }
+        public double Percent { get; private set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter the amount of the meal:");
+            Amount = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the percent you want tip to tip:");
+            Percent = double.Parse(Console.ReadLine());
+        }
+        public void Output()
+        {
+
+        }
+    }
+}
